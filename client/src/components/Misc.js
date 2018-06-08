86b83c57fc752736ca32e423c5ca14accf92a1c8
javrd <trarsheise@gmail.com>
2018-06-08 16:05:31 +0200

    Modal in progress

--- client/src/components/Misc.js
+++ client/src/components/Misc.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { nameChange } from '../sockets';
+import edit from '../Edit_icon.svg';
 import { FINISHED } from '../utils';
 
 const Misc = ({  state, time }) => {    
@@ -25,12 +26,32 @@ const Misc = ({  state, time }) => {
             <div>Ronda: {state.round}</div>
             <div>Estado del jugador: {state.playerState}</div>
             <div>Estado de la sala: {state.state}</div>
-            <div className="form-inline justify-content-center" >
-            <div className="form-group mx-sm-3 mb-2">
-              <input type="text" className="form-control" id="nombre_form" placeholder="Nuevo nombre" />
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#nickModal">
+              <img src={edit}/>
+            </button>
+            {/* Modal */}
+            <div className="modal fade" id="nickModal" tabindex="-1" role="dialog" aria-labelledby="nickModalLabel" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title text-dark" id="nickModalLabel">¡Cambia tu nombre!</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+
+                    <div className="form-inline justify-content-center" >
+                      <div className="form-group mx-sm-3 mb-2">
+                        <input type="text" className="form-control" id="nombre_form" placeholder="Nuevo nombre" />
+                      </div>
+                    </div>
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" className="btn btn-primary" data-dismiss="modal" onClick={() => nameChange()}>Cambiar</button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <button type="submit" className="btn btn-primary mb-2" onClick={() => nameChange()}>Enviar</button>
-          </div>
           </div>
           :
           <div>

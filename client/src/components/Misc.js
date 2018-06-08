7bd1ad92d1ba8f91b6bb5ec75721be8097788c5b
daniglper <daniglper@gmail.com>
2018-06-08 17:05:15 +0200

    Seccion Misc cambiada para partida en estado LOBBY

--- client/src/components/Misc.js
+++ client/src/components/Misc.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { nameChange } from '../sockets';
-import { FINISHED } from '../utils';
+import { FINISHED, IN_GAME } from '../utils';
+import loading from '../loading.gif';
 
 const Misc = ({  state, time }) => {    
   let winners;
@@ -12,41 +13,68 @@ const Misc = ({  state, time }) => {
     let player4 = {name: state.player4.name, score: state.player4.score};
     winners = [player1,player2,player3,player4];
     winners.sort((a,b) => a.score<b.score);
-  }
 
-  return (
-    <div className="card text-white bg-success mb-3">
-      <div className="card-body">
-        <p className="card-text">
-          {state.state!==FINISHED ?
-          <div>
-            <div>Tiempo: {time}</div>
-            <div>Turno: {state.turn}</div>
-            <div>Ronda: {state.round}</div>
-            <div>Estado del jugador: {state.playerState}</div>
-            <div>Estado de la sala: {state.state}</div>
-            <div className="form-inline justify-content-center" >
-            <div className="form-group mx-sm-3 mb-2">
-              <input type="text" className="form-control" id="nombre_form" placeholder="Nuevo nombre" />
+    return (
+      <div className="card text-white bg-success mb-3">
+        <div className="card-body">
+          <p className="card-text">
+            <div>
+              {winners.map((w, i) =>{
+                if(i===0){
+                  return <h3 key={w.name+w.score+i}>{i+1}. {w.name} - {w.score} </h3>
+                }else{
+                  return <div key={w.name+w.score+i}>{i+1}. {w.name} - {w.score} </div>
+                }
+              })}
             </div>
-            <button type="submit" className="btn btn-primary mb-2" onClick={() => nameChange()}>Enviar</button>
-          </div>
-          </div>
-          :
-          <div>
-            {winners.map((w, i) =>{
-              if(i===0){
-                return <h3 key={w.name+w.score+i}>{i+1}. {w.name} - {w.score} </h3>
-              }else{
-                return <div key={w.name+w.score+i}>{i+1}. {w.name} - {w.score} </div>
-              }
-            })}
-          </div>
-          }  
-        </p>
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if(state.state===IN_GAME){
+    return (
+      <div className="card text-white bg-success mb-3">
+        <div className="card-body">
+          <p className="card-text">
+            <div>
+              <div>Tiempo: {time}</div>
+              <div>Turno: {state.turn}</div>
+              <div>Ronda: {state.round}</div>
+              <div>Estado del jugador: {state.playerState}</div>
+              <div>Estado de la sala: {state.state}</div>
+              <div className="form-inline justify-content-center" >
+              <div className="form-group mx-sm-3 mb-2">
+                <input type="text" className="form-control" id="nombre_form" placeholder="Nuevo nombre" />
+              </div>
+              <button type="submit" className="btn btn-primary mb-2" onClick={() => nameChange()}>Enviar</button>
+            </div>
+            </div>
+          </p>
+        </div>
+      </div>
+    );
+  } else{//LOBBY
+    let count = 1;
+    if(state.player2.name){
+      count++;
+    }
+    if(state.player3.name){
+      count++;
+    }
+    if(state.player4.name){
+      count++;
+    }
+    return (
+      <div className="card text-white bg-success mb-3">
+        <div className="card-body">
+          <p className="card-text">
+            Esperando a otros jugadores {count}/4
+            <img src={loading}  width="40"/>
+          </p>
+        </div>
+      </div>
+    );
+  }
 };
 
 Misc.propTypes = {
